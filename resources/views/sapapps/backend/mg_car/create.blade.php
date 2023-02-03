238d16b756f88dc8a21df93f07368a6db237d231
minpo22rs <minpo22rs@gmail.com>
2023-02-03 16:47:59 +0700

    t

---
+++ resources/views/sapapps/backend/mg_car/create.blade.php
@@ -0,0 +1,233 @@
+@extends('sapapps.backend.inc.template')
+
+@push('styles')
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{!! asset('public/backend/app-assets/vendors/css/vendors.min.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('public/backend/app-assets/vendors/css/forms/wizard/bs-stepper.min.css') !!}">
+    <link rel="stylesheet" type="text/css" href="{!! asset('public/backend/app-assets/vendors/css/forms/select/select2.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('public/backend/app-assets/vendors/css/forms/form-validation.css') !!}">
+    <link rel="stylesheet" href="{!! asset('public/backend/app-assets/vendors/css/forms/form-wizard.css') !!}">
+    <!-- END: Vendor CSS-->
+@endpush
+
+@section('content')
+    <div class="app-content content ">
+                            @include('sapapps.backend.inc.alert')
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            <div class="content-header row">
+                <div class="content-header-left col-md-9 col-12 mb-2">
+                    <div class="row breadcrumbs-top">
+                        <div class="col-12">
+                            <h2 class="content-header-title float-start mb-0">{!! $menu !!}</h2>
+                            <div class="breadcrumb-wrapper">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">รถยนต์</a>
+                                    </li>
+                                    <li class="breadcrumb-item active">เพิ่มข้อมูล
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <!-- Horizontal Wizard -->
+                <!-- /Horizontal Wizard -->
+
+                <!-- Vertical Wizard -->
+                <form action="{{url('/backend/'.$url)}}" method="POST" enctype="multipart/form-data">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <section class="vertical-wizard">
+                        <div class="bs-stepper vertical vertical-wizard-example">
+                            <div class="bs-stepper-header">
+                                <div class="step" data-target="#account-details-vertical" role="tab" id="account-details-vertical-trigger">
+                                    <button type="button" class="step-trigger">
+                                        <span class="bs-stepper-box">1</span>
+                                        <span class="bs-stepper-label">
+                                            <span class="bs-stepper-title">ข้อมูลรถยนต์</span>
+                                            <span class="bs-stepper-subtitle">รายละเอียดของรถยนต์</span>
+                                        </span>
+                                    </button>
+                                </div>
+                                <div class="step" data-target="#personal-info-vertical" role="tab" id="personal-info-vertical-trigger">
+                                    <button type="button" class="step-trigger">
+                                        <span class="bs-stepper-box">2</span>
+                                        <span class="bs-stepper-label">
+                                            <span class="bs-stepper-title">ข้อมูลราคารถยนต์</span>
+                                            <span class="bs-stepper-subtitle">สกุลเงินบาทไทย</span>
+                                        </span>
+                                    </button>
+                                </div>
+                                <div class="step" data-target="#address-step-vertical" role="tab" id="address-step-vertical-trigger">
+                                    <button type="button" class="step-trigger">
+                                        <span class="bs-stepper-box">3</span>
+                                        <span class="bs-stepper-label">
+                                            <span class="bs-stepper-title">รูปภาพรถยนต์</span>
+                                            <span class="bs-stepper-subtitle">รูปปกและรูปสไลด์</span>
+                                        </span>
+                                    </button>
+                                </div>
+                                <!-- <div class="step" data-target="#social-links-vertical" role="tab" id="social-links-vertical-trigger">
+                                    <button type="button" class="step-trigger">
+                                        <span class="bs-stepper-box">4</span>
+                                        <span class="bs-stepper-label">
+                                            <span class="bs-stepper-title">Social Links</span>
+                                            <span class="bs-stepper-subtitle">Add Social Links</span>
+                                        </span>
+                                    </button>
+                                </div> -->
+                            </div>
+                            <div class="bs-stepper-content">
+                                <div id="account-details-vertical" class="content" role="tabpanel" aria-labelledby="account-details-vertical-trigger">
+                                    <div class="content-header">
+                                        <h5 class="mb-0">ข้อมูลรถยนต์</h5>
+                                        <small class="text-muted">กรอกรายละเอียดรถยนต์</small>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-category">ประเภทรถยนต์</label>
+                                            <select class="form-select" name="category" id="vertical-category">
+                                                {!! \App\Model\dashboard::category_option_html(( old('category')),'Select','car') !!}
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-car-name">ชื่อรถยนต์</label>
+                                            <input type="text" name="car_name" id="vertical-car-name" class="form-control" value="{!! old('car_name') !!}">
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-short-descrip">รายละเอียดของรถยนต์โดยย่อ</label>
+                                            <textarea name="short_descrip" id="vertical-short-descrip" class="form-control">{!! old('short_descrip') !!}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-ful-descrip">รายละเอียดของรถยนต์</label>
+                                            <textarea name="full_descrip" id="vertical-ful-descrip" class="form-control">{!! old('full_descrip') !!}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <button type="button" class="btn btn-outline-secondary btn-prev" disabled>
+                                            <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                                            <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                                        </button>
+                                        <button type="button" class="btn btn-primary btn-next">
+                                            <span class="align-middle d-sm-inline-block d-none">Next</span>
+                                            <i data-feather="arrow-right" class="align-middle ms-sm-25 ms-0"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div id="personal-info-vertical" class="content" role="tabpanel" aria-labelledby="personal-info-vertical-trigger">
+                                    <div class="content-header">
+                                        <h5 class="mb-0">ข้อมูลราคารถยนต์</h5>
+                                        <small>กรอกรายละเอียดราคารถยนต์</small>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-unit-price">ราคารถยนต์ต่อหน่วย</label>
+                                            <input type="text" name="unit_price" id="vertical-unit-price" class="form-control" value="{!! old('unit_price') !!}">
+                                        </div>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-sale-price">ราคาขายรถยนต์</label>
+                                            <input type="text" name="sale_price" id="vertical-sale-price" class="form-control" value="{!! old('sale_price') !!}">
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <button type="button" class="btn btn-primary btn-prev">
+                                            <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                                            <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                                        </button>
+                                        <button type="button" class="btn btn-primary btn-next">
+                                            <span class="align-middle d-sm-inline-block d-none">Next</span>
+                                            <i data-feather="arrow-right" class="align-middle ms-sm-25 ms-0"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div id="address-step-vertical" class="content" role="tabpanel" aria-labelledby="address-step-vertical-trigger">
+                                    <div class="content-header">
+                                        <h5 class="mb-0">รูปภาพรถยนต์</h5>
+                                        <small>ลงรูปภาพรถยนต์</small>
+                                    </div>
+                                    <div class="row form-group">
+                                        <div class="mb-1 col-md-12">
+                                            <label class="form-label" for="vertical-cover-img">รูปปกรถยนต์</label>
+                                            <input type="file" name="car_img_name" id="vertical-cover-img" class="form-control" value="{!! old('car_img_name') !!}">
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <button type="button" class="btn btn-primary btn-prev">
+                                            <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                                            <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                                        </button>
+                                        <button type="submit" class="btn btn-success btn-submit">Submit</button>
+                                    </div>
+                                </div>
+                                <!-- <div id="social-links-vertical" class="content" role="tabpanel" aria-labelledby="social-links-vertical-trigger">
+                                    <div class="content-header">
+                                        <h5 class="mb-0">Social Links</h5>
+                                        <small>Enter Your Social Links.</small>
+                                    </div>
+                                    <div class="row">
+                                        <div class="mb-1 col-md-6">
+                                            <label class="form-label" for="vertical-twitter">Twitter</label>
+                                            <input type="text" id="vertical-twitter" class="form-control" placeholder="https://twitter.com/abc" />
+                                        </div>
+                                        <div class="mb-1 col-md-6">
+                                            <label class="form-label" for="vertical-facebook">Facebook</label>
+                                            <input type="text" id="vertical-facebook" class="form-control" placeholder="https://facebook.com/abc" />
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="mb-1 col-md-6">
+                                            <label class="form-label" for="vertical-google">Google+</label>
+                                            <input type="text" id="vertical-google" class="form-control" placeholder="https://plus.google.com/abc" />
+                                        </div>
+                                        <div class="mb-1 col-md-6">
+                                            <label class="form-label" for="vertical-linkedin">Linkedin</label>
+                                            <input type="text" id="vertical-linkedin" class="form-control" placeholder="https://linkedin.com/abc" />
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <button type="button" class="btn btn-primary btn-prev">
+                                            <i data-feather="arrow-left" class="align-middle me-sm-25 me-0"></i>
+                                            <span class="align-middle d-sm-inline-block d-none">Previous</span>
+                                        </button>
+                                        <button type="submit" class="btn btn-success btn-submit">Submit</button>
+                                    </div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </section>
+                </form>
+                <!-- /Vertical Wizard -->
+
+
+
+            </div>
+        </div>
+    </div>
+@stop
+
+
+@push('scripts')
+
+<!-- BEGIN: Page Vendor JS-->
+<script src="{!! asset('public/backend/app-assets/vendors/js/forms/wizard/bs-stepper.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/forms/select/select2.full.min.js') !!}"></script>
+<!-- <script src="{!! asset('public/backend/app-assets/vendors/js/forms/validation/jquery.validate.min.js') !!}"></script> -->
+<!-- END: Page Vendor JS-->
+
+<!-- BEGIN: Page JS-->
+<script src="{!! asset('public/backend/app-assets/js/scripts/forms/form-wizard.js') !!}"></script>
+<!-- END: Page JS-->
+@endpush
+
+
---
+++ resources/views/sapapps/backend/mg_car/imageslide.blade.php
@@ -0,0 +1,210 @@
+@extends('sapapps.backend.inc.template')
+
+@push('styles')
+<!-- Data Table Css -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+<!-- Custom css -->
+<link rel="stylesheet" type="text/css" href="{!! asset('resources/css/config_datatables.css') !!}">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/v4-shims.css">
+@endpush
+
+
+@section('content')
+    <div class="app-content content ">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            <div class="content-header row">
+                <div class="content-header-left col-md-9 col-12 mb-2">
+                    <div class="row breadcrumbs-top">
+                        <div class="col-12">
+                            <h2 class="content-header-title float-start mb-0">{{$menu}}</h2>
+                            <div class="breadcrumb-wrapper">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="#">รถยนต์</a>
+                                    </li>
+                                    <li class="breadcrumb-item"><a href="#">{{$car}}</a>
+                                    </li>
+                                    <li class="breadcrumb-item"><a href="#">รูปภาพรถยนต์</a>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <div class="col-md-12 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">เพิ่มรูปภาพรถยนต์</h4>
+                        </div>
+                        <div class="card-body">
+                            <form class="form form-horizontal" action="{{url('/backend/'.$url.'/'.$id.'/imageslide')}}" method="POST" enctype="multipart/form-data">
+                                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <input type="hidden" name="type" value="car">
+                                <div class="row">
+                                    <div class="col-10">
+                                        <div class="mb-1 row">
+                                            <div class="col-sm-12">
+                                                <label class="col-form-label" for="slide_img_name">รูปภาพรถยนต์</label>
+                                            </div>
+                                            <div class="col-sm-12">
+                                                <input type="file" id="slide_img_name" class="form-control" name="slide_img_name" value=""/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="mb-1 row form-group">
+                                            <div class="col-sm-12 py-1">
+                                                <button type="submit" class="btn btn-primary me-1">Submit</button>
+                                            </div>
+                                            <div class="col-sm-12">
+                                                <button type="reset" class="btn btn-outline-secondary">Reset</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- Basic table -->
+                <section id="basic-datatable">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <table id="data-estate" class="datatables-basic table">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>รูปภาพรถยนต์</th>
+                                            <th>วันที่ลงข้อมูล</th>
+                                            <th>วันที่อัพเดตข้อมูล</th>
+                                            <th>ผู้ลงข้อมูล</th>
+                                            <th>การจัดการ</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--/ Basic table -->
+
+            </div>
+        </div>
+    </div>
+@stop
+
+
+@push('scripts')
+
+<!-- BEGIN: Vendor JS-->
+<script src="{!! asset('public/backend/app-assets/vendors/js/vendors.min.js') !!}"></script>
+<!-- BEGIN Vendor JS-->
+
+<!-- BEGIN: Page Vendor JS-->
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/jquery.dataTables.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/dataTables.bootstrap5.min.js') !!}"></script>
+<!-- <script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/dataTables.responsive.min.js') !!}"></script> -->
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/responsive.bootstrap5.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/datatables.buttons.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/jszip.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/pdfmake.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/vfs_fonts.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/buttons.html5.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/buttons.print.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/tables/datatable/dataTables.rowGroup.min.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js') !!}"></script>
+<!-- END: Page Vendor JS-->
+
+<!-- BEGIN: Theme JS-->
+<script src="{!! asset('public/backend/app-assets/js/core/app-menu.js') !!}"></script>
+<script src="{!! asset('public/backend/app-assets/js/core/app.js') !!}"></script>
+<!-- END: Theme JS-->
+
+<!-- BEGIN: Page JS-->
+<script src="{!! asset('public/backend/app-assets/js/scripts/tables/table-datatables-basic.js') !!}"></script>
+<!-- END: Page JS-->
+
+<script>
+    $(window).on('load', function() {
+        if (feather) {
+            feather.replace({
+                width: 14,
+                height: 14
+            });
+        }
+    })
+</script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        var ajax = {
+            "url": '{!! route($url.'.datatables_image') !!}',
+            "type": "POST",
+            'headers': {
+                'X-CSRF-TOKEN': '{!! csrf_token() !!}'
+            },
+            'data': {
+                'id' : '{!! $id !!}'
+            }
+        };
+        $('#data-estate').dataTable({
+            paging: true,
+            searching: true,
+            scrollX: false,
+            scrollํY: true,
+            autoWidth: false,
+            order: [[0, "asc"],[0, "desc"]],
+            fixedHeader: {
+                "header": false,
+                "footer": false
+            },
+            dom: '<"html5buttons"B>lTfgtipr',
+            buttons: [],
+            ajax: ajax,
+            columns: [
+                { data: 'id',
+                    render: function(data) {
+                        return data;
+                    },
+                },
+                { data: 'slide_img_name',
+                    render: function(data) {
+                        return data;
+                    },
+                },
+                { data: 'created_at',
+                    render: function(data) {
+                        return data;
+                    },
+                },
+                { data: 'updated_at',
+                    render: function(data) {
+                        return data;
+                    },
+                },
+                { data: 'updated_at_ref_admin_id',
+                    render: function(data) {
+                        return data;
+                    },
+                },
+                { data: 'manage',
+                    render: function(data) {
+                        return data;
+                    },
+                },
+            ],
+        });
+
+    });
+</script>
+@endpush
+
+
+
