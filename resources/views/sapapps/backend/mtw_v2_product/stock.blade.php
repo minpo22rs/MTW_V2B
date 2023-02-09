10de6255c3ebe1646903f623e2d01a02753c00ea
minpo22rs <minpo22rs@gmail.com>
2023-02-09 13:46:36 +0700

    ee

--- resources/views/sapapps/backend/mtw_v2_product/stock.blade.php
+++ resources/views/sapapps/backend/mtw_v2_product/stock.blade.php
@@ -25,7 +25,7 @@
                             <div class="card">
                                 <div class="card-block">
                                     <h5 class="m-b-10">{{ $menu }} > Stock > {!! $rec->product_name !!}</h5>
-                                    <p class="text-muted m-b-10"><code>ข้อมูdddddล</code></p>
+                                    <p class="text-muted m-b-10"><code>ข้อมูล</code></p>
                                     <ul class="breadcrumb-title b-t-default p-t-10">
                                         <li class="breadcrumb-item">
                                             <a href="{!! url("backend/$url") !!}"> <i class="fa fa-home"></i> </a>
